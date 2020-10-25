e9d937304d1e9cab7153d51d6c3c93290e80ac29
EvgeniaYurchik <52360296+EvgeniaYurchik@users.noreply.github.com>
2020-10-25 16:03:55 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        let pos = n * (-1);
+        let a = String(pos);
+        return a.split("").reverse().join("");
+    } else {
+        let a = String(n);
+        return a.split("").reverse().join("");
+    }
 }
